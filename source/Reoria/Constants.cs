4739d57aae85150ec4648a639528aea47e7ed80e
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-09-13 14:16:12 -0400

    Created source solution files.

---
+++ source/Reoria/Constants.cs
@@ -0,0 +1,21 @@
+﻿namespace Reoria
+{
+    /// <summary>
+    /// Defines constants used by the game for various functions.
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Defines the version of Reoria that this build was created on.
+        /// </summary>
+        public const string REORIA_VERSION = "0.1.0";
+        /// <summary>
+        /// Defines the version of Godot that this build was created on.
+        /// </summary>
+        public const string GODOT_VERSION = "4.1.1";
+        /// <summary>
+        /// Defines the version of dotnet that this build was created on.
+        /// </summary>
+        public const string DOTNET_VERSION = "net6.0";
+    }
+}
