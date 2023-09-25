0a9fa121dd684aaf34b11ac3caafeb1472e2189f
XerShade <jonathan.campbell.1992@hotmail.co.uk>
2023-09-25 00:45:44 -0400

    Added node properties.

--- source/Reoria.Game/Entities/AnimatedEntity.cs
+++ source/Reoria.Game/Entities/AnimatedEntity.cs
@@ -1,9 +1,18 @@
-﻿using Reoria.Game.Entities.Interfaces;
+﻿using Godot;
+using Reoria.Game.Entities.Interfaces;
 
 namespace Reoria.Game.Entities
 {
     public partial class AnimatedEntity : StaticEntity, IAnimatedEntity
     {
+        [Export]
+        public AnimatedSprite2D animatedSprite { get; protected set; }
 
+        public override void _Ready()
+        {
+            base._Ready();
+
+            animatedSprite = GetNode<AnimatedSprite2D>("AnimatedSprite") ?? throw new NullReferenceException("Unable to instantiate StaticEntity node: Could not find component AnimatedSprite2D.");
+        }
     }
 }
--- source/Reoria.Game/Entities/StaticEntity.cs
+++ source/Reoria.Game/Entities/StaticEntity.cs
@@ -1,9 +1,21 @@
-﻿using Reoria.Game.Entities.Interfaces;
+﻿using Godot;
+using Reoria.Game.Entities.Interfaces;
 
 namespace Reoria.Game.Entities
 {
     public partial class StaticEntity : Entity, IStaticEntity
     {
+        [Export]
+        public Sprite2D sprite { get; protected set; }
+        [Export]
+        public Node2D collisionCalculator { get; protected set; }
 
+        public override void _Ready()
+        {
+            base._Ready();
+
+            sprite = GetNode<Sprite2D>("Sprite") ?? throw new NullReferenceException("Unable to instantiate StaticEntity node: Could not find component Sprite2D.");
+            collisionCalculator = GetNode<Node2D>("SpriteCollisionCalculator") ?? throw new NullReferenceException("Unable to instantiate StaticEntity node: Could not find component Node2D.");
+        }
     }
 }
