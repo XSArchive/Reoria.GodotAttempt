25fb0b66d99e8b4c918674175419025d865e6bde
XerShade <xershade.ca@gmail.com>
2023-09-26 04:34:29 -0400

    Added actor node.

---
+++ source/nodes/actors/actor_collision_shape.gd
@@ -0,0 +1,12 @@
+extends CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_sprite_texture_changed():
+	pass # Replace with function body.
